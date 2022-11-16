61027ff54117a2b31e55d86236537d357a60ca97
KauanAzevedo <kauan.azevedo@fontespromotora.com.br>
2022-11-16 16:59:52 -0300

    initial commit

--- js/script.js
+++ js/script.js
@@ -1,9 +1,22 @@
-function gotoInicio() {
-  document.getElementById("header").scrollIntoView();
+const toggleSwitch = document.querySelector('#checkbox');
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light';
+
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark'){
+        toggleSwitch.checked = true;
+    }
 }
-function gotoProblema() {
-  document.getElementById("problema").scrollIntoView();
+
+function switchTheme(e) {
+    if(e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+    }
 }
-function gotoProposta() {
-  document.getElementById("proposta").scrollIntoView();
-}
+toggleSwitch.addEventListener('click', switchTheme);
